619add09a702c70199040e36f954487aae0ee08c
jon dupont <jondupont@gmail.com>
2018-07-22 00:58:05 -0400

    First Commit
    First commit after a few videos

---
+++ Assets/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        //Find the Ridged Body 
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            print("pressing A Only");
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("pressing D Only");
+        }
+    }
+}
